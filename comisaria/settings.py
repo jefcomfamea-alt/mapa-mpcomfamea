fb433ecd5e3274e648abec5f5d6ac670ad261316
jefcomfamea-alt <TU_CORREO_DE_GITHUB>
2026-07-07 16:58:03 -0500

    Agregar autenticación con login

--- comisaria/settings.py
+++ comisaria/settings.py
@@ -59,14 +59,17 @@ ROOT_URLCONF = 'comisaria.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.csrf",
+                "django.template.context_processors.static",
             ],
         },
     },
@@ -129,3 +132,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
--- comisaria/urls.py
+++ comisaria/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mapa.views import inicio
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', inicio),
+    path("admin/", admin.site.urls),
+
+    # Login y logout de Django
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    # Página principal (mapa)
+    path("", inicio, name="inicio"),
 ]
